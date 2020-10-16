0610dffbc26f1ee57126acfec34da45401cceed5
Romit Singh Bhau <romitenvious@icloud.com>
2020-10-16 10:12:52 +0530

    Add files via upload

---
+++ src/Post2.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Post2.css";
+
+function Post2() {
+return (
+
+<div class="flex-container">
+  <div class="flex-container2">
+  <img alt="" draggable="true" src="https://pbs.twimg.com/profile_images/1311910979983212544/4GPRicQv_bigger.jpg" class="avatar"/>
+</div>
+
+  <div class="flex-container3">
+  <span>
+  Regan Trump<span class="post-username"> @ReganTrump</span>
+  </span>
+  <p class="post-body"><span class="post-replyingto">Replying to</span> @DonaldTrump</p>
+  <p class="post-body">I know somebody who’s in bed </p>
+  
+<img alt="Image" draggable="true" src="https://pbs.twimg.com/media/EkWBrdqUcAM4b3w?format=jpg&amp;name=small" class="post-img" />
+  </div>  
+</div>
+
+);
+}
+
+export default Post2;
